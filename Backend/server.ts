8cad68e76519d76f976440f6eaf5d2001a3e3dce
KarthikeyaTadiparti <kartikeyatadiparti@gmail.com>
2025-08-15 11:51:15 +0530

    connected mongodb

--- Backend/server.ts
+++ Backend/server.ts
@@ -1,11 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
+import connectDB from "./config/db.js";
 
 dotenv.config();
+connectDB();
 const app = express();
 const PORT = process.env.PORT;
-
 
 app.use(cors());
 app.use(express.json());
