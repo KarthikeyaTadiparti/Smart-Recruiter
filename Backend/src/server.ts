70ee3be3a1c79d4e411184ac1d4a86d5709f46b8
KarthikeyaTadiparti <kartikeyatadiparti@gmail.com>
2026-01-28 14:12:00 +0530

    connect redis

--- Backend/src/server.ts
+++ Backend/src/server.ts
@@ -2,39 +2,46 @@ import "dotenv/config";
 import cors from "cors";
 import express from "express";
 import cookieParser from "cookie-parser";
+
 import { errorHandler, handle404Error } from "./middlewares/errorhandler.ts";
 import authRoutes from "./routes/auth-routes.ts";
 import companyRoutes from "./routes/company-routes.ts";
 import jobRoutes from "./routes/job-routes.ts";
-import morganMiddleware from "./config/morgan.ts";
-import db from "./config/db.ts";
-import { users } from "./schema/users-schema.ts";
 import interviewRoutes from "./routes/interview-routes.ts";
 import feedbackRoutes from "./routes/applications-routes.ts";
+
+import morganMiddleware from "./config/morgan.ts";
+import db from "./config/db.ts";
+import { connectRedis } from "./config/redis.ts";
+import { users } from "./schema/users-schema.ts";
 
 const app = express();
 const { PORT, APP_URL } = process.env;
 
+/* -------------------- CORS -------------------- */
 const corsOptions = {
-    origin: [APP_URL as string],
-    credentials: true,
+  origin: [APP_URL as string],
+  credentials: true,
 };
 
+/* -------------------- MIDDLEWARES -------------------- */
 app.use(morganMiddleware);
 app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-app.use('/auth',authRoutes);
-app.use('/companies',companyRoutes);
-app.use('/jobs',jobRoutes);
-app.use('/interviews',interviewRoutes);
-app.use('/feedbacks',feedbackRoutes);
+/* -------------------- ROUTES -------------------- */
+app.use("/auth", authRoutes);
+app.use("/companies", companyRoutes);
+app.use("/jobs", jobRoutes);
+app.use("/interviews", interviewRoutes);
+app.use("/feedbacks", feedbackRoutes);
 
+/* -------------------- TEST ROUTE -------------------- */
 app.get("/test", async (req, res) => {
   try {
-    const rows = await db.select().from(users); 
+    const rows = await db.select().from(users);
     res.json(rows);
   } catch (err) {
     console.error("DB error:", err);
@@ -42,9 +49,24 @@ app.get("/test", async (req, res) => {
   }
 });
 
-app.use(handle404Error)
+/* -------------------- ERROR HANDLERS -------------------- */
+app.use(handle404Error);
 app.use(errorHandler);
 
-app.listen(PORT, () => {
-    console.log(`server is listening on port ${PORT}`);
-});
+/* -------------------- SERVER STARTUP -------------------- */
+const startServer = async () => {
+  try {
+    //Connect Redis
+    await connectRedis();
+
+    //Start server
+    app.listen(PORT, () => {
+      console.log(`Server is listening on port ${PORT}`);
+    });
+  } catch (error) {
+    console.error("Server startup failed:", error);
+    process.exit(1);
+  }
+};
+
+startServer();
